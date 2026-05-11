c374e3fdd69f108d837cb68ec6d51c644d0dddd2
taikisora <tobuk@bokuno.com>
2026-05-11 17:16:28 +0900

    force sync

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ Filter, ChevronRight, ExternalLink
 
 const SUPABASE_URL = 'https://gftwcfexduvwgvffigbg.supabase.co';
 
-const SUPABASE_ANON_KEY = 'sb_publishable_MbxHPGc62c9GY8pZwWmcmQ_L10kZhkf';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmdHdjZmV4ZHV2d2d2ZmZpZ2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzOTUzNTYsImV4cCI6MjA5Mzk3MTM1Nn0.SIUtKv-tvLqBbP0VLHf0QIV3tZBZSPtKzXjR2Tyad8c';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
