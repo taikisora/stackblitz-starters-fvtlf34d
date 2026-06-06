641e0462a4c3e075f0fd7246b11ee8521dc134c5
taikisora <tobuk@bokuno.com>
2026-06-06 19:45:31 +0900

    force sync

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',          // Next.jsを静的ファイルとして書き出す
-    trailingSlash: true,       // 💡 アプリ内でページ遷移（リンク移動）を正常に動かすための必須設定
-    images: { unoptimized: true }, // 💡 アプリ化の際、画像最適化エラーでビルドが止まるのを防ぐ設定
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+module.exports = nextConfig;
